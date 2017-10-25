482f0cc5c1ba90478322d288a15fa75c21ce8ba7
wpupru <web@wpup.ru>
2017-10-25 14:34:25 -0500

    Создание таблиц и мигриции

---
+++ database/migrations/2017_10_25_190922_change_articles_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class ChangeArticlesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('articles', function (Blueprint $table) {
+
+            $table->string('alias')->default('alias')->change();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('articles', function (Blueprint $table) {
+
+          //  $table->dropColumn('alias');
+
+        });
+    }
+}
